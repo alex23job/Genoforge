c66c7b8cfcee2a3f37f74f593472e12929efee3a
alex23job <106354657+alex23job@users.noreply.github.com>
2026-03-21 21:44:59 +0300

    Adding canera movement

---
+++ GeneforgeChampions/Assets/Scripts/BattleGame/CameraMovement.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private float _maxLeft;
+    [SerializeField] private float _maxRight;
+    [SerializeField] private Scrollbar _cameraScrollbar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraScrollbar.value = 0.3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnValueChanged()
+    {
+        float sumMax = _maxRight - _maxLeft;
+        Vector3 cameraPosition = transform.position;
+        float posX = _maxLeft + _cameraScrollbar.value * sumMax;
+        cameraPosition.x = Mathf.Clamp(posX, _maxLeft, _maxRight);
+        transform.position = cameraPosition;
+    }
+}
